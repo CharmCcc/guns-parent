6cad9f8e11942f7bc9f806996c86acf9539b2214
charmingchm <842721197@qq.com>
2019-12-26 20:34:50 +0800

    权限认证测试完成

--- guns-user/src/main/java/com/stylefeng/guns/rest/modular/user/UserAPIImpl.java
+++ guns-user/src/main/java/com/stylefeng/guns/rest/modular/user/UserAPIImpl.java
@@ -2,6 +2,8 @@ package com.stylefeng.guns.rest.modular.user;
 
 import com.alibaba.dubbo.config.annotation.Service;
 import com.stylefeng.guns.api.user.UserAPI;
+import com.stylefeng.guns.api.user.UserInfoModel;
+import com.stylefeng.guns.api.user.UserModel;
 import org.springframework.stereotype.Component;
 
 /**
@@ -12,8 +14,28 @@ import org.springframework.stereotype.Component;
 @Service(interfaceClass = UserAPI.class)
 public class UserAPIImpl implements UserAPI {
     @Override
-    public boolean login(String username, String password) {
+    public int login(String username, String password) {
         System.out.println(username+"OKKKKKKK"+password);
+        return 1;
+    }
+
+    @Override
+    public boolean register(UserModel userModel) {
         return false;
+    }
+
+    @Override
+    public boolean checkUsername(String username) {
+        return false;
+    }
+
+    @Override
+    public UserInfoModel getUserInfo(int uuid) {
+        return null;
+    }
+
+    @Override
+    public UserInfoModel updateUserInfo(UserInfoModel userInfoModel) {
+        return null;
     }
 }
